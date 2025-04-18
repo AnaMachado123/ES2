3734adc863e0851a82ef53054bb9759637de57d8
adrianameira <adriana.meira@ipvc.pt>
2025-04-18 15:01:00 +0100

    Alteração Convite.cs

--- back/back/Models/Convite.cs
+++ back/back/Models/Convite.cs
@@ -1,7 +1,7 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BackendTesteESII.Models;
+namespace BackendTesteESII.Models.DTOs;
 
 [Table("convite")]
 public class Convite
