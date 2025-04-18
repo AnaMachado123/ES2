e15f43089816b613a4d424c408d14555c89f34d4
eduardagomes12-admin <eduarda.gomes@ipvc.pt>
2025-04-18 15:25:59 +0100

    UtilizadorCreateDTO mudança

--- back/back/Models/DTOs/UtilizadorCreateDTO.cs
+++ back/back/Models/DTOs/UtilizadorCreateDTO.cs
@@ -5,6 +5,7 @@ namespace BackendTesteESII.Models.DTOs;
 
 public class UtilizadorCreateDTO
 {
+    public required string Tipo { get; set; } 
     public string Nome { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
