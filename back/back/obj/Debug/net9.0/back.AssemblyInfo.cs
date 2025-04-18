228c9eb3309d0dc22749d69a10ab41388cc93c1c
eduardagomes12-admin <eduarda.gomes@ipvc.pt>
2025-04-18 14:56:43 +0100

    obj

--- back/back/obj/Debug/net9.0/back.AssemblyInfo.cs
+++ back/back/obj/Debug/net9.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841fd599e0a0c16aa020fe6be3b10878629afb92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2fb5121dab3fd0b37a356c6f60ade1bd4c080ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
