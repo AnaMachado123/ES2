424e30c470d76b22406269211bf6d2f23ff24785
eduardagomes12-admin <eduarda.gomes@ipvc.pt>
2025-04-25 16:11:34 +0100

    Nova migration para fazer relaçao entre tarefas e projeto

--- back/back/Migrations/20250425102402_AddTarefasToProjeto.Designer.cs
+++ back/back/Migrations/20250425102402_AddTarefasToProjeto.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BackendTesteESII.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BackendTesteESII.Migrations
 {
     [DbContext(typeof(GestaoServicosClientesContext))]
-    partial class GestaoServicosClientesContextModelSnapshot : ModelSnapshot
+    [Migration("20250425102402_AddTarefasToProjeto")]
+    partial class AddTarefasToProjeto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ back/back/Migrations/20250425102402_AddTarefasToProjeto.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendTesteESII.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTarefasToProjeto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_tarefa_projeto_id",
+                table: "tarefa",
+                column: "projeto_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tarefa_projeto_projeto_id",
+                table: "tarefa",
+                column: "projeto_id",
+                principalTable: "projeto",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tarefa_projeto_projeto_id",
+                table: "tarefa");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tarefa_projeto_id",
+                table: "tarefa");
+        }
+    }
+}
