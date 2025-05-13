d43aeec4b87614d44d5b149c6af24132e664986c
adrianameira <adriana.meira@ipvc.pt>
2025-05-13 19:28:34 +0100

    Criaçao de uma Migration para ligar os utilizadores às tarefas

---
+++ back/back/Migrations/20250513171308_AddUtilizadorNavigationToTarefa.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendTesteESII.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUtilizadorNavigationToTarefa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_tarefa_utilizador_id",
+                table: "tarefa",
+                column: "utilizador_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tarefa_utilizador_utilizador_id",
+                table: "tarefa",
+                column: "utilizador_id",
+                principalTable: "utilizador",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tarefa_utilizador_utilizador_id",
+                table: "tarefa");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tarefa_utilizador_id",
+                table: "tarefa");
+        }
+    }
+}
